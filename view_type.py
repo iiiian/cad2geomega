2f9c550f55f90fd3b392b3f24dbaeb6cb1cc9d4c
iiiian <thisisianemail@gmail.com>
2020-11-13 18:39:03 +0800

    .

---
+++ view_type.py
@@ -0,0 +1,15 @@
+import re
+
+with open('wall.STEP','r') as wall:
+    line=wall.readline()
+    typ=[]
+    while line:
+        mat=re.findall(r"#\d+ = ([A-Z_]+)",line)
+        #print(mat)
+        if mat:
+            typ.append(mat[0])
+        line=wall.readline()
+
+
+typ=set(typ)
+print(typ)
